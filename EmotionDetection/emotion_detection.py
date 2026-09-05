c0a5173de62b44bd11c37d2f16cdee71143966d2
eddiejh03 <eddiejh03@users.noreply.github.com>
2026-09-05 14:07:31 -0400

    Complete error handling and PyLint compliance

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,6 +7,18 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
     
     response = requests.post(url, json=myobj, headers=headers)
+    
+    # Check if the server response is 400 (Bad Request / blank text)
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+        
     formatted_response = json.loads(response.text)
     
     emotions = formatted_response['emotionPredictions'][0]['emotion']
